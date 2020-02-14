00cc0da1e690890fcee50e69c1f7bdb66ef69a16
Alex <aggout.dev@gmail.com>
2020-02-14 17:22:21 -0500

    added combine

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from script.drive import Drive
 from script.steer import Steer
 from script.rotate import Rotate
+from script.combine import *
 from rolocode.rolo import Rolo
 from nxt_main.nxt_brick import NXTBrickController
 import os
@@ -28,6 +29,7 @@ class Main:
         self.drive_script = Drive(rolo)
         self.steer_script = Steer(rolo)
         self.rotate_script = Rotate(rolo)
+        self.combine_script = Combine(rolo)
 
     def test_drive1(self):
         params = {'power': 100, 'time': 2}
@@ -77,8 +79,48 @@ class Main:
         params = {'power': 120, 'dir': 'right', 'time': 1}
         self.rotate_script.execute(params)
 
+    def combine1(self):
+        straight1 = CParams(100, 100, 2)
+        straight2 = CParams(75, 75, 2)
+        turn1 = CParams(75, 100, 2)
+        turn2 = CParams(75, 50, 2)
+
+        params = {'straight1': straight1, 'straight2': straight2, 'turn1': turn1, 'turn2': turn2}
+        self.combine_script.execute(params)
+
+
+def unit_tests(_main):
+    _main.test_drive1()
+    time.sleep(t_sleep)
+    _main.test_drive2()
+    time.sleep(t_sleep)
+    _main.test_drive3()
+    time.sleep(t_sleep)
+    _main.test_drive4()
+    time.sleep(t_sleep)
+    _main.test_steer1()
+    time.sleep(t_sleep)
+    _main.test_steer2()
+    time.sleep(t_sleep)
+    _main.test_steer3()
+    time.sleep(t_sleep)
+    _main.test_steer4()
+    time.sleep(t_sleep)
+    _main.test_rotate1()
+    time.sleep(t_sleep)
+    _main.test_rotate2()
+    time.sleep(t_sleep)
+    _main.test_rotate3()
+    time.sleep(t_sleep)
+    _main.test_rotate4()
+
 
 t_sleep = 1
+
+
+def combine_tests(_main):
+    _main.combine1()
+
 
 if __name__ == '__main__':
     brick_connection = None
@@ -88,29 +130,9 @@ if __name__ == '__main__':
         print(str(brick_connection))
         rolo = Rolo(NXTBrickController(brick_connection))
         m = Main(rolo)
-        m.test_drive1()
-        time.sleep(t_sleep)
-        m.test_drive2()
-        time.sleep(t_sleep)
-        m.test_drive3()
-        time.sleep(t_sleep)
-        m.test_drive4()
-        time.sleep(t_sleep)
-        m.test_steer1()
-        time.sleep(t_sleep)
-        m.test_steer2()
-        time.sleep(t_sleep)
-        m.test_steer3()
-        time.sleep(t_sleep)
-        m.test_steer4()
-        time.sleep(t_sleep)
-        m.test_rotate1()
-        time.sleep(t_sleep)
-        m.test_rotate2()
-        time.sleep(t_sleep)
-        m.test_rotate3()
-        time.sleep(t_sleep)
-        m.test_rotate4()
+        # unit_tests(m)
+        combine_tests(m)
+
     except IOError:
         print("Error while running tests:")
         print(str(sys.exc_info()[1]))
--- script/combine.py
+++ script/combine.py
@@ -2,11 +2,26 @@ import time
 from script.script import Script
 
 
-class Drive(Script):
-    def do_execute(self, params):
-        power = params['power']
-        secs = params['time']
+class CParams:
+    def __init__(self, power_l, power_r, duration):
+        self.power_l = power_l
+        self.power = power_l
+        self.power_r = power_r
+        self.duration = duration
 
-        self.rolo.drive_straight(power)
-        time.sleep(secs)
-        self.rolo.halt()
+
+class Combine(Script):
+    def do_execute(self, params):
+        straight1 = params['straight1']
+        straight2 = params['straight2']
+        turn1 = params['turn1']
+        turn2 = params['turn2']
+
+        self.rolo.drive_straight(straight1.power)
+        time.sleep(straight1.duration)
+        self.rolo.drive(turn1.power_l, turn1.power_r)
+        time.sleep(turn1.duration)
+        self.rolo.drive_straight(straight2.power)
+        time.sleep(straight2.duration)
+        self.rolo.drive(turn2.power_l, turn2.power_r)
+        time.sleep(turn2.duration)
